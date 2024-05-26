b0e3b086fd4830444e84db16f70120578ed2a507
Charity <charitywanjiku8245@gmail.com>
2024-05-26 12:12:04 +0300

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,34 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    
+    def __init__(self,name='name',breed='Chihuahua'):
+        self.name = name
+        self.breed = breed
+        
+    def get_name(self):
+        return self._name    
+    
+    def set_name(self,name):
+        if ((type(name) is str) and (len(name) > 1 and len(name) < 25)):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    name = property(get_name,set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self,breed):
+        self._breed = None
+        for approvedBreed in APPROVED_BREEDS:
+            if breed == approvedBreed:
+                self._breed = breed
+                break
+        if self._breed == None:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed,set_breed)                       
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name = 'john doe',job = 'Sales'):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+        pass
+
+    def set_name(self,name):
+        if (type(name) is str) and (len(name) > 1 and len(name) < 25):
+            name = name.title()
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.") 
+
+    name = property(get_name,set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self,job):
+        self._job = None
+        for approvedJob in APPROVED_JOBS:
+            if job == approvedJob:
+                self._job = job
+        if self._job == None:
+            print("Job must be in list of approved jobs.")        
+        pass
+
+    job =  property(get_job,set_job)
+Jane = Person()
+print(Jane.name)
